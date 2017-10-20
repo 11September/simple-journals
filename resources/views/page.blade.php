1e9c8efd6983dafe87b063d06d5b2c4e96134a99
Stanislav <shiptya@icloud.com>
2017-10-20 11:21:06 +0300

    add some check if text and footer

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,7 +1,14 @@
 @extends('layoults.layoult')
 
 @section('css')
-
+    @if(iconv_strlen($page->body) < 2500)
+        <style>
+            .footer{
+                position: absolute;
+                bottom: 0;
+            }
+        </style>
+    @endif
 @endsection
 
 @section('content')
@@ -18,6 +25,7 @@
 
             <div class="content">
                 {!! $page->body !!}
+                {!! iconv_strlen($page->body) !!}
             </div>
         </div>
     </div>
