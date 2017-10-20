030ff52cc4d6ef00e7386f7340557f2b3c8781b7
Stanislav <shiptya@icloud.com>
2017-10-20 16:27:12 +0300

    markup to the new blade

--- resources/views/advertisement.blade.php
+++ resources/views/advertisement.blade.php
@@ -101,6 +101,23 @@
                         <hr>
 
                         <p>total price is: <span id="total_price">50 $</span></p>
+
+                        <div class="clearfix"></div>
+
+                        <form>
+                            <div class="row">
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="First name">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="First name">
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Last name">
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        </form>
                     </div>
                 @endif
 
