40de43d0f50667989cdf70c3474c14a43bfd89c0
Stanislav <shiptya@icloud.com>
2017-10-23 16:44:56 +0300

    add meta tags

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,7 +7,7 @@
 <meta name="description" content="{{ Voyager::setting('description') }}">
 <meta name="keywords" content="@yield('keywords')">
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="author" content="Shiptya Stanislav">
+<meta name="author" content="Demid Poskachey">
 
-
-<meta name="author" content="">
-<link rel="icon" href="favicon.ico">
+<link rel="icon" href="{{ asset('favicon.ico') }}">
