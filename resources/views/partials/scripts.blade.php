77b5664af9fb028377f8193404482bee32c4f006
Stanislav <shiptya@icloud.com>
2017-10-25 12:32:42 +0300

    css optimize

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,10 +1,6 @@
 <!-- <script type='text/javascript' src='{{ asset('js/jquery-3.2.1.slim.min.js') }}'></script> -->
-<script
-			  src="https://code.jquery.com/jquery-3.2.1.min.js"
-			  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-			  crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 <script type='text/javascript' src='{{ asset('js/popper.min.js') }}'></script>
 <script type='text/javascript' src='{{ asset('js/bootstrap.min.js') }}'></script>
 <script type='text/javascript' src='{{ asset('js/ie10-viewport-bug-workaround.js') }}'></script>
 <script src="https://www.paypalobjects.com/api/checkout.js"></script>
-{{--<script type="text/javascript" src='{{ asset('js/main.js') }}'></script>--}}
