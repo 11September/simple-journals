5817cb060d33ef91a55080f08d4d20a996d68dc8
Stanislav <shiptya@icloud.com>
2017-10-25 12:29:36 +0300

    css optimize

--- resources/views/partials/css.blade.php
+++ resources/views/partials/css.blade.php
@@ -1,4 +1,4 @@
 <link rel='stylesheet' href='{{ asset('css/bootstrap.min.css') }}' type='text/css' media='all'/>
-<link rel='stylesheet' href='{{ asset('css/style.css') }}' type='text/css' media='all'/>
+<link rel='stylesheet' href='{{ asset('css/style.min.css') }}' type='text/css' media='all'/>
 
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css'/>
