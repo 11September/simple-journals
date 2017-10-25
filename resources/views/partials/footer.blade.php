444a338bcd1262fd04c54890cf6be0f1e2f99d5a
Stanislav <shiptya@icloud.com>
2017-10-25 14:27:21 +0300

    some fix errors and add style.css

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="container-fluid footer">
 	<div class="row footer-wrapper">
-		<p class="footer-text">Created by <a class="copyright-link" href="http://uat-solutions.com/" target="_blank">UAT</a></p>
+		<p class="footer-text">All rights reserved 2017</p>
 	</div>
 </footer>
