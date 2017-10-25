69d0c2e88a68c5bb4a3ade4c448489815cb02ef5
Stanislav <shiptya@icloud.com>
2017-10-25 16:31:46 +0300

    more changes new patch

--- resources/views/partials/css.blade.php
+++ resources/views/partials/css.blade.php
@@ -1,4 +1,4 @@
 <link rel='stylesheet' href='{{ asset('css/bootstrap.min.css') }}' type='text/css' media='all'/>
 <link rel='stylesheet' href='{{ asset('css/style.css') }}' type='text/css' media='all'/>
-
+{{--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">--}}
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' type='text/css'/>
