bb02ce125ec85d6284e366806450005673feae36
Stanislav <shiptya@icloud.com>
2018-03-30 14:07:52 +0300

    keywords

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,12 +2,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" href="{{ asset('images/favicon/cropped-cropped-logo-1-32x32.png') }}" sizes="16x16" type="image/png">
 
-<title>{{ Voyager::setting('title') }}</title>
+<title>{{ Voyager::setting('site.title') }}</title>
+<meta name="description" content="{{ Voyager::setting('site.description') }}">
+<meta name="keywords" content="{{ Voyager::setting('site.keywords') }}">
 
-<meta name="description" content="{{ Voyager::setting('description') }}">
-<meta name="keywords" content="@yield('keywords')">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="author" content="Shiptya Stanislav">
-<meta name="author" content="Demid Poskachey">
 
 <link rel="icon" href="{{ asset('favicon.ico') }}">
